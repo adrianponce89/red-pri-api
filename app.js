45365a5efaeef4514baa6f9ee4cea9967df58723
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-07-03 17:06:56 -0300

    Add database connection

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // app.js
-
+require('dotenv').config({ path: __dirname + '/.env' });
 const express =  require('express');
+const connectDB = require('./config/db');
 
+// Initializations
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
+// Settings
 const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log(`Server runninf on port ${port}`));
+// DB Connection
+connectDB();
+
+// Routes
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// Start server
+app.listen(port, () => console.log(`Server running on port ${port}`));
