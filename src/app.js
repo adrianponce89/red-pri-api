cb82cdc5175e051703627a1059e021fdb63246aa
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-07-10 13:33:59 -0300

    Run next and express simultaneously

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// app.js
+require('dotenv').config();
+const express =  require('express');
+const http = require('http');
+const next = require("next");
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Settings
+const port = process.env.PORT || 8082;
+const dev = process.env.NODE_ENV !== "production";
+
+// DB Connection
+connectDB();
+
+// Initializations
+const app = next({
+  dev,
+  dir: "./src"
+});
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Middlewares
+  server.use(morgan('dev'));
+  server.use(express.json());
+  
+  // importing routes
+  const articles = require('./routes/articles');
+  
+  // Routes
+  server.use('/articles', articles);
+  
+  // handling everything else with Next.js
+  server.get("*", app.getRequestHandler());
+  
+  // Start server
+  http.createServer(server).listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
