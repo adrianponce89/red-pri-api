9b08caad32f09ccc5805c2dafac66d8cefa8e6d2
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-07-15 01:00:55 -0300

    Save created article
    TODO: save creator id on article register

---
+++ src/components/Loadable.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import Button from "react-bootstrap/Button";
+import Spinner from "./Spinner";
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: ${props => props.loading? 'flex' : 'none'};
+  justify-content: center;
+  align-items: center;
+`;
+const ChildrenContainer = styled.div`
+  visibility: ${props => props.loading ? 'hidden' : 'inherit'};
+`;
+
+const Loadable = (WrappedComponent) => ({ loading, size, dark, color, children, ...otherProps }) => (
+    <WrappedComponent {...otherProps} style={{ position: 'relative' }}>
+      <ChildrenContainer loading={loading}>
+        {children}
+      </ChildrenContainer>
+      <SpinnerContainer loading={loading}>
+        <Spinner size={size} dark={dark} color={color}/>
+      </SpinnerContainer>
+    </WrappedComponent>
+)
+
+export const LoadableButton = Loadable(Button);
+
+export default Loadable;
--- src/pages/crear-articulo.js
+++ src/pages/crear-articulo.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import Router from 'next/router'
 import styled from 'styled-components';
+import fetch from "isomorphic-fetch";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,76 +10,121 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Editor } from '@tinymce/tinymce-react';
 import { tinyAPIKey } from '../config';
+import { LoadableButton } from '../components/Loadable';
 
 const CrearArticulo = ({ className }) => {
+  const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [category, setCategory] = useState("PSICOLOGIA");
+  const [tags, setTags] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleEditorChange = (content, editor) => {
-    setContent(content);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const res = await fetch("/api/articles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title,
+        content,
+        category,
+        tags
+      })
+    });
+    
+    if (res.status === 201) {
+      Router.push('/articulos');
+    } else {
+      const resJson = await res.json();
+      alert(resJson.error);
+      setLoading(false);
+    } 
   }
+
   return (
     <Container className={className}>
-      <Row>
-        <Col lg="8" className="pb-2">
-          <Card>
-            <Card.Body>
-              <Form>
-                <Form.Group controlId="title">
-                  <Form.Label>Título</Form.Label>
-                  <Form.Control type="text" placeholder="Título (obligatorio)" />
-                </Form.Group>
-                <Form.Group controlId="content">
-                  <Editor
-                    apiKey={`${tinyAPIKey}`}
-                    initialValue={content}
-                    init={{
-                      height: 500,
-                      menubar: false,
-                      language: 'es',
-                      plugins: [
-                        'advlist autolink lists link image',
-                        'fullscreen',
-                        'media table help wordcount'
-                      ],
-                      toolbar:
-                        'fullscreen | undo redo | formatselect | bold italic underline | \
-                        alignleft aligncenter alignright alignjustify | \
-                        bullist numlist outdent indent | removeformat | link table image media | help'
-                    }}
-                    onEditorChange={handleEditorChange}
-                  />
-                </Form.Group>
-              </Form>
-            </Card.Body>
-          </Card> 
-        </Col>
-        <Col lg="4" className="pb-2">
-          <Card>
-            <Card.Header>Completa la publicación</Card.Header>
-            <Card.Body>
-              <Form>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                  <Form.Label>Categoria</Form.Label>
-                  <Form.Control as="select">
-                    <option>PSICOLOGIA</option>
-                    <option>EDUCACION</option>
-                    <option>LITERATURA INFANTIL</option>
-                    <option>HISTORIAS</option>
-                    <option>SALUD Y NUTRICION</option>
-                    <option>EMBARAZO Y MATERNIDAD</option>
-                    <option>OCIO Y RECREACION</option>
-                  </Form.Control>
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Label>Etiquetas</Form.Label>
-                  <Form.Control type="text" placeholder="Ingresa etiquetas separándolas por comas." />
-                </Form.Group>
-                <Button type="submit" block>Publicar</Button>
-              </Form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col lg="8" className="pb-2">
+            <Card>
+              <Card.Body>
+                  <Form.Group controlId="title">
+                    <Form.Label>Título</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={title} 
+                      onChange={e => setTitle(e.target.value)}
+                      placeholder="Título (obligatorio)"
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="content">
+                    <Editor
+                      apiKey={`${tinyAPIKey}`}
+                      initialValue={content}
+                      init={{
+                        height: 300,
+                        menubar: false,
+                        language: 'es',
+                        plugins: [
+                          'advlist autolink lists link image',
+                          'fullscreen',
+                          'media table help wordcount'
+                        ],
+                        toolbar:
+                          'fullscreen | undo redo | formatselect | bold italic underline | \
+                          alignleft aligncenter alignright alignjustify | \
+                          bullist numlist outdent indent | removeformat | link table image media | help'
+                      }}
+                      onEditorChange={content => setContent(content)}
+                    />
+                  </Form.Group>
+              </Card.Body>
+            </Card> 
+          </Col>
+          <Col lg="4" className="pb-2">
+            <Card>
+              <Card.Header>Completa la publicación</Card.Header>
+              <Card.Body>
+                  <Form.Group controlId="exampleForm.ControlSelect1">
+                    <Form.Label>Categoria</Form.Label>
+                    <Form.Control
+                      as="select"
+                      value={category} 
+                      onChange={e => setCategory(e.target.value)}
+                    >
+                      <option>PSICOLOGIA</option>
+                      <option>EDUCACION</option>
+                      <option>LITERATURA INFANTIL</option>
+                      <option>HISTORIAS</option>
+                      <option>SALUD Y NUTRICION</option>
+                      <option>EMBARAZO Y MATERNIDAD</option>
+                      <option>OCIO Y RECREACION</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Etiquetas</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={tags} 
+                      onChange={e => setTags(e.target.value)}
+                      placeholder="Ingresa etiquetas separándolas por comas."
+                    />
+                  </Form.Group>
+                  <LoadableButton
+                    loading={loading}
+                    disabled={loading}
+                    type="submit"
+                    block>
+                    Publicar
+                  </LoadableButton>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Form>
     </Container>
   )
 };
