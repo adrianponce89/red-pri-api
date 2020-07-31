182aef4e7aefab24c9dee3181f6dfafb55072a06
Roger Coverzola <diolink.4103@gmail.com>
2020-07-31 19:59:54 -0300

    CarBox Component Finished

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,36 +1,56 @@
 import styled from 'styled-components';
 
+export const WIDTH_SM = '576px';
+export const WIDTH_FLUID = '100%';
+export const WIDTH_PORCENT = '30%';
+export const HEIGHT_OUTERSIZE = '324.75px';
+export const HEIGHT_FLUID = '100%';
+export const PADDING_BOTTOM = '161.8%';
+export const MARGIM_TOP = '10px';
+export const MARGIM_RIGTH = '10px';
+export const NONE = '0';
+export const SHADOW = '2px 2px 4px 0px rgba(0, 0, 0, 0.3)';
+
 const OuterSize = styled.div`
-  height: 324.75px;
-  width: 30%;
+  width: ${WIDTH_PORCENT};
+  height: ${(props) =>
+    props.hight ? HEIGHT_OUTERSIZE : HEIGHT_FLUID};
   position: relative;
-  margin: 8px 8px;
+  margin: ${MARGIM_TOP} ${MARGIM_RIGTH};
   background-color: white;
+  box-shadow: ${(props) => (props.shadow ? SHADOW : NONE)};
   overflow: hidden;
-  @media (max-width: 576px) {
-    width: 100%;
+  @media (max-width: ${WIDTH_SM}) {
+    width: ${WIDTH_FLUID};
   }
 `;
 
 const InnerSize = styled.div`
-  padding-bottom: 161.8%;
+  padding-bottom: ${PADDING_BOTTOM};
 `;
 
 const FloatingContainer = styled.div`
   position: absolute;
-  left: 0;
-  right: 0;
-  top: 10px;
-  bottom: 0;
+  left: ${NONE};
+  right: ${NONE};
+  top: ${(props) => props.floatingtop || NONE};
+  bottom: ${NONE};
 `;
 
-const CardBox = (prorp) => {
+const CardBox = (props) => {
   return (
-    <OuterSize>
+    <OuterSize shadow={props.boxshadow} hight={props.hightoutersize}>
       <InnerSize />
       <FloatingContainer>{props.children}</FloatingContainer>
     </OuterSize>
   );
 };
 
-export default styled(CardBox);
+export default styled(CardBox)`
+  margin-bottom: 1em;
+  padding: 0;
+  overflow: hidden;
+  p {
+    margin: 0;
+  }
+`;
