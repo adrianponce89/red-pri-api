d080a5046056180e46c83d68b1d9e8df69e27fbc
Roger Coverzola <diolink.4103@gmail.com>
2020-08-03 17:22:23 -0300

    Corrections are made to the CarBox file, according to the provision of the PR.

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,47 +1,43 @@
 import styled from 'styled-components';
 
-export const WIDTH_SM = '576px';
-export const WIDTH_FLUID = '100%';
-export const WIDTH_PORCENT = '30%';
-export const HEIGHT_OUTERSIZE = '324.75px';
-export const HEIGHT_FLUID = '100%';
-export const PADDING_BOTTOM = '161.8%';
-export const MARGIM_TOP = '10px';
-export const MARGIM_RIGTH = '10px';
-export const NONE = '0';
-export const SHADOW = '2px 2px 4px 0px rgba(0, 0, 0, 0.3)';
+const WIDTH_SM = '576px';
+const OUTERSIZE_PROPORTION = '30%';
+const OUTERSIZE_MARGIN = '10px 10px';
+const PADDING_BOTTOM = '161.8%';
 
 const OuterSize = styled.div`
-  width: ${WIDTH_PORCENT};
-  height: ${(props) =>
-    props.hight ? HEIGHT_OUTERSIZE : HEIGHT_FLUID};
+  width: ${OUTERSIZE_PROPORTION};
   position: relative;
-  margin: ${MARGIM_TOP} ${MARGIM_RIGTH};
+  margin: ${OUTERSIZE_MARGIN};
   background-color: white;
-  box-shadow: ${(props) => (props.shadow ? SHADOW : NONE)};
+  box-shadow: ${(props) => props.shadow};
   overflow: hidden;
   @media (max-width: ${WIDTH_SM}) {
-    width: ${WIDTH_FLUID};
+    width: 100%;
   }
 `;
 
 const InnerSize = styled.div`
   padding-bottom: ${PADDING_BOTTOM};
+  display: flex;
+  justify-content: center;
 `;
 
 const FloatingContainer = styled.div`
   position: absolute;
-  left: ${NONE};
-  right: ${NONE};
-  top: ${(props) => props.floatingtop || NONE};
-  bottom: ${NONE};
+  left: 0;
+  right: 0;
+  top: ${(props) => props.floatingtop};
+  bottom: 0;
 `;
 
 const CardBox = (props) => {
   return (
-    <OuterSize shadow={props.boxshadow} hight={props.hightoutersize}>
+    <OuterSize {...props}>
       <InnerSize />
-      <FloatingContainer>{props.children}</FloatingContainer>
+      <FloatingContainer floatingtop={props.floatingtop}>
+        {props.children}
+      </FloatingContainer>
     </OuterSize>
   );
 };
