d1733ecd24096506ed33c592218a3bbc00db4c9a
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-08-29 14:56:44 -0300

    Add search again message

--- src/pages/busqueda/[...params].js
+++ src/pages/busqueda/[...params].js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import fetch from 'isomorphic-fetch';
+import { Marker } from '@react-google-maps/api';
 import { server } from '../../config';
 import { hyphenToSpace, getKeysFromSlugParams } from '../../utils';
 import Container from '../../components/Container';
@@ -14,7 +15,7 @@ import AvailableFilters from '../../components/Search/AvailableFilters';
 import FAIcon from '../../components/FAIcon';
 import NavPills from '../../components/NavPills';
 import ResultsMap from '../../components/ResultsMap';
-import { Marker } from '@react-google-maps/api';
+import NoResults from '../../components/Search/NoResults';
 
 const FiltersTitle = styled.h4`
   margin: 0.2em 0;
@@ -84,7 +85,9 @@ const Busqueda = ({ results, filters, paging, availableFilters }) => {
               ]}
             />
           </div>
-          {resultType === 'list' ? (
+          {results.length === 0 ? (
+            <NoResults />
+          ) : resultType === 'list' ? (
             <div>
               {results.map((user) => (
                 <ProfesionalCard key={user._id} {...user} />
