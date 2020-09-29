b8ca908b96049abbb831233545caddc60b684866
Adrian Ponce <adrian.ponce.89@gmail.com>
2020-09-28 22:18:07 -0300

    Add categories filter, message on profile creation

--- src/pages/articulos/index.js
+++ src/pages/articulos/index.js
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import fetch from 'isomorphic-fetch';
 import { useSelector } from 'react-redux';
@@ -12,6 +14,12 @@ import FAIcon from '../../components/FAIcon';
 
 const Articulos = ({ articles, popular }) => {
   const profile = useSelector((state) => state.auth.profile);
+  const [category, setCategory] = useState('');
+
+  const filteredArticles = category.length
+    ? articles.filter((article) => article.category === category)
+    : articles;
+
   return (
     <Container>
       <Row>
@@ -20,19 +28,23 @@ const Articulos = ({ articles, popular }) => {
         </Col>
         <Col md={{ span: 8, order: 'first' }}>
           <div className="d-flex justify-content-between pb-2">
-            <NavPills
-              defaultActiveKey="/articulos#destacados"
-              items={[
-                {
-                  href: '/articulos#destacados',
-                  title: 'Destacados',
-                },
-                {
-                  href: '/articulos#recientes',
-                  title: 'Recientes',
-                },
-              ]}
-            />
+            <Form.Group inline controlId="inlineFormCategory">
+              <Form.Control
+                as="select"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Categorias...</option>
+                <option>PSICOLOGIA</option>
+                <option>EDUCACION</option>
+                <option>LITERATURA INFANTIL</option>
+                <option>HISTORIAS</option>
+                <option>SALUD Y NUTRICION</option>
+                <option>EMBARAZO Y MATERNIDAD</option>
+                <option>OCIO Y RECREACION</option>
+              </Form.Control>
+            </Form.Group>
+
             {profile && profile.permits && profile.permits.writes ? (
               <Button
                 variant="success"
@@ -50,7 +62,7 @@ const Articulos = ({ articles, popular }) => {
             )}
           </div>
           <div>
-            {articles.map((article) => (
+            {filteredArticles.map((article) => (
               <ArticleCard key={article._id} {...article} />
             ))}
           </div>
