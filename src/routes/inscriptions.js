dc4109e25d63d216ccfa2127fe5942de5cd47f37
Roger Coverzola <diolink.4103@gmail.com>
2021-01-28 17:11:40 -0300

    The enrollment path file is created.

---
+++ src/routes/inscriptions.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = require('express-promise-router');
+const { restrictAccess } = require('../utils/access');
+const InscriprionsController = require('../controllers/inscriptions');
+
+router
+  .route('/')
+  .get(restrictAccess('admin'), InscriprionsController.index)
+  .post(InscriprionsController.newInscription);
+
+router
+  .route('/:inscriptionsId')
+  .delete(
+    restrictAccess('admin'),
+    InscriprionsController.removeInscription,
+  );
+
+module.exports = router;
